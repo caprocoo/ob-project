faffa08f316f5a5ac4b728dcfdf02c2c9eb9dd97
caprocoo <dhgustjd10@gmail.com>
2022-12-08 13:54:09 +0900

    first

---
+++ src/test/java/com/caprocoo/ob/ObApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caprocoo.ob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
