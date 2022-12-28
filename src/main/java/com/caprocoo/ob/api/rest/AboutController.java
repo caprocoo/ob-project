bf60a101d5f53ca0dc3fcaffe3f82714412fd25f
caprocoo <dhgustjd10@gmail.com>
2022-12-28 14:16:40 +0900

    page divide, css purple

--- src/main/java/com/caprocoo/ob/api/rest/AboutController.java
+++ src/main/java/com/caprocoo/ob/api/rest/AboutController.java
@@ -3,38 +3,29 @@ package com.caprocoo.ob.api.rest;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
 
 /**
- * packageName    : com.caprocoo.ob.controller
- * fileName       : HomeController
+ * packageName    : com.caprocoo.ob.api.rest
+ * fileName       : AlbumController
  * author         : caprocoo
- * date           : 2022-12-09
+ * date           : 2022-12-28
  * description    :
  * ===========================================================
  * DATE              AUTHOR             NOTE
  * -----------------------------------------------------------
- * 2022-12-09        caprocoo       최초 생성
+ * 2022-12-28        caprocoo       최초 생성
  */
-
-//@RestController
 @Controller
-@RequestMapping("/main")
-public class HomeController {
+@RequestMapping("/about")
+public class AboutController {
 
     @RequestMapping(value = "", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public String home(Model model){
-        HashMap<String, String> hello = new HashMap<>();
-        hello.put("title", "OBONG");
-        model.addAttribute("hello", hello);
-        return "index";
+    public String aboutHome(Model model){
+
+        return "about/main";
     }
-
-
 
 }
