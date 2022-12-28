f1601d415ad9e6bfa57b42bd642d28e595ccbb10
caprocoo <dhgustjd10@gmail.com>
2022-12-28 10:56:01 +0900

    jsp & bootstrap 적용

--- src/main/java/com/caprocoo/ob/api/rest/HomeController.java
+++ src/main/java/com/caprocoo/ob/api/rest/HomeController.java
@@ -1,6 +1,9 @@
 package com.caprocoo.ob.api.rest;
 
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,35 +22,18 @@ import java.util.HashMap;
  * 2022-12-09        caprocoo       최초 생성
  */
 
-@RestController
-@RequestMapping("/api/test")
+//@RestController
+@Controller
+@RequestMapping("/main")
 public class HomeController {
+
     @RequestMapping(value = "", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public HashMap<String, String> home(){
+    public String home(Model model){
         HashMap<String, String> hello = new HashMap<>();
         hello.put("title", "OBONG");
-        return hello;
+        model.addAttribute("hello", hello);
+        return "index";
     }
 
-    @RequestMapping(value = "/news", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public HashMap<String, String> news(){
-        HashMap<String, String> news = new HashMap<>();
-        news.put("title", "news");
-        return news;
-    }
-
-    @RequestMapping(value = "/ask", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public HashMap<String, String> ask(){
-        HashMap<String, String> ask = new HashMap<>();
-        ask.put("title", "ask");
-        return ask;
-    }
-
-    @RequestMapping(value = "/jobs", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public HashMap<String, String> jobs(){
-        HashMap<String, String> jobs = new HashMap<>();
-        jobs.put("title", "jobs");
-        return jobs;
-    }
 
 }
--- src/main/java/com/caprocoo/ob/config/SecurityConfig.java
+++ src/main/java/com/caprocoo/ob/config/SecurityConfig.java
@@ -42,7 +42,7 @@ public class SecurityConfig  {
                 .and()
                 .authorizeRequests()
                 // 해당 API에 대해서는 모든 요청을 허가한다는 설정
-                .antMatchers("/api/member/**").permitAll()
+                .antMatchers("/**").permitAll()
 //                // USER 권한이 있어야 요청할 수 있다는 설정
 //                .antMatchers("/api/rez").hasRole("USER")
                 // 이 밖에 모든 요청에 대해서 인증을 필요로 한다는 설정
